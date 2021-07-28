668610041a41263a5b383b0349da0005597f242a
Nikhil Rachawar <nikhilrachawar100@gmail.com>
2021-07-28 08:26:31 +0530

    linked In started with no JS

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDwq-tcw9nS84VyWYoWgBwMu_LgdFg3iag",
+    authDomain: "nikhil-linkedin-clone.firebaseapp.com",
+    projectId: "nikhil-linkedin-clone",
+    storageBucket: "nikhil-linkedin-clone.appspot.com",
+    messagingSenderId: "704900920022",
+    appId: "1:704900920022:web:d5703d10ad12bc5f6b0f77",
+    measurementId: "G-0HB5DSP3F0"
+  };
